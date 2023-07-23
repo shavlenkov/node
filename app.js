6ca7b105b7224bc68c634e66a776814dddf780fd
Pavel Shavlenkov <pashashavlenkov@gmail.com>
2023-07-23 17:05:30 +0300

    a

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ app.use(bodyParser.json());
 
 app.use('/static', express.static(__dirname + '/public'));
 
-app.post('/pay', (req, res) => {
-    const summa = req.body.summa;
+app.get('/pay', (req, res) => {
+    const summa = req.query.summa;
 
     // Формируем URL виджета Qiwi
-    const url = 'https://my.qiwi.com/form/Nykolai-SNzEVfSaLk?amount=' + summa;
+    //const url = 'https://oplata.qiwi.com/create?publicKey=48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPwQrNQsye6f5uRiENxbZVuJVuNLVLvckbePDrL6UVYLTy2wzujgWvkUrRvd7zZTBYWegcuMEx8bkLyn7ps52AwfDaU2gfdDFamf94dvQT8&amount=' + summa;
+const url = 'https://oplata.qiwi.com/create?publicKey=48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPwQrNQsye6f5uRiENxbZVuJVuNLVLvckbePDrL6UVYLTy2wzujgWvkUrRvd7zZTBYWegcuMEx8bkLyn7ps52AwfDaU2gfdDFamf94dvQT8&amount=' + summa
+
 
     res.redirect(url);
 });
